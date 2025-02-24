601eb79829283dd0fe4e94c08cd048f62fd0c8b8
M Shahzad <muhammadshahzad45435@gmail.com>
2025-02-24 16:57:58 +0500

    first comment

---
+++ day_1/app.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------- #
+#?                                    Day_1                                    #
+# ---------------------------------------------------------------------------- #
+
+
+#* Print Your First Message!
+
+
+my_name = "Muhammad Shahzad"
+print(f"Hello, Python Learners! 🚀 I am {my_name}.Today is the first step in our coding journey.")
+
+
+#output :Hello, Python Learners! 🚀 I am Muhammad Shahzad.Today is the first step in our coding journey. 
