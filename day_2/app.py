62cce1363c3277c411d6d981ddb9c7b33550dec4
M Shahzad <muhammadshahzad45435@gmail.com>
2025-02-25 07:20:04 +0500

    first comment

---
+++ day_2/app.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------- #
+#                                     Day_2                                    #
+# ---------------------------------------------------------------------------- #
+
+user_input = str(input("Enter any messages :"))
+
+word_count = len(user_input.split())
+print(f"Total words :{word_count}")
+
+
+
+#output : Enter any messages :I love Python programming
+# Total words :4
