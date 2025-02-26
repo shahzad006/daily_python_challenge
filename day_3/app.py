d9d77b2bf6832e9ea558e9cf404dec2b94557c37
M Shahzad <muhammadshahzad45435@gmail.com>
2025-02-26 18:16:38 +0500

    first comment

---
+++ day_3/app.py
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------- #
+#                                     day_3                                    #
+# ---------------------------------------------------------------------------- #
+"""
+🚀 Challenge: Aisa Python program likho jo user se ek number le aur check kare ke wo prime number hai ya nahi! 🔢💡
+
+🔥 Example:
+📌 Input: 7
+📌 Output: Yes, it's a prime number!
+📌 Input: 10
+📌 Output: No, it's not a prime number!
+
+🔥 Think you can do it? Let’s go! 💪
+
+"""
+
+
+user_input = int(input("Enter any number : "))
+
+if user_input % 2 == 0:
+    print("No It's not prime number!")
+
+else:
+    print("Yes It's a prime number!")
+
+
+# Output :
+'''
+Enter any number : 7
+Yes It's a prime number!
+
+Enter any number : 9
+Yes It's a prime number!
+
+Enter any number : 5
+Yes It's a prime number!
+'''
+
